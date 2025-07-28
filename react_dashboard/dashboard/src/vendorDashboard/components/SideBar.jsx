3bc1394610f66e3a00539fa16d0ba4473c8bb11b
Harsha <tejuharsha1024@gmail.com>
2025-07-28 12:32:20 +0530

    Frontend

---
+++ react_dashboard/dashboard/src/vendorDashboard/components/SideBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div className='sideBarSection'>
+        <ul>
+            <li>Add Firm</li>
+            <li>Add Product</li>
+            <li>All Products</li>
+            <li>User Details</li>
+        </ul>
+    </div>
+  )
+}
+
+export default SideBar
---
+++ react_dashboard/dashboard/src/vendorDashboard/pages/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import SideBar from '../components/SideBar'
+
+const LandingPage = () => {
+  return (
+    <>
+        <section className="landingSection">
+            <NavBar />
+            <SideBar />
+        </section>
+    </>
+  )
+}
+
+export default LandingPage
